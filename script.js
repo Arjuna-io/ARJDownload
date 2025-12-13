0e36711fa458ef19e0d6a6119126d20333d25a62
Arjuna-io <nurarjunadiazsyahputra05@gmail.com>
2025-12-13 10:39:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function ig(){
+  let v=document.getElementById("ig").value;
+  if(!v)return alert("Masukkan link Instagram");
+  open("https://snapinsta.app/");
+}
+
+function fb(){
+  let v=document.getElementById("fb").value;
+  if(!v)return alert("Masukkan link Facebook");
+  open("https://fdownloader.net/");
+}
+
+function yt(){
+  let v=document.getElementById("yt").value;
+  if(!v)return alert("Masukkan link YouTube");
+  open("https://yt1s.com/");
+}
