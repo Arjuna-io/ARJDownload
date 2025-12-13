8c90e4a76f4a0e6be1082fb963012de6f31d9f0a
Arjuna-io <nurarjunadiazsyahputra05@gmail.com>
2025-12-13 11:18:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function detect(){
   const input = document.getElementById("url");
-
   if(!input){
-    alert("Input URL tidak ditemukan");
+    alert("Input tidak ditemukan");
     return;
   }
 
   const url = input.value.trim();
-
   if(url === ""){
     alert("Masukkan link video");
     return;
   }
+
+  navigator.clipboard.writeText(url);
 
   if(url.includes("tiktok.com")){
     window.open(
@@ -20,12 +20,15 @@ function detect(){
     );
   }
   else if(url.includes("instagram.com")){
+    alert("Link disalin. Tempelkan di igram.world");
     window.open("https://igram.world/en/", "_blank");
   }
   else if(url.includes("facebook.com") || url.includes("fb.watch")){
+    alert("Link disalin. Tempelkan di fdownloader");
     window.open("https://fdownloader.net/", "_blank");
   }
   else if(url.includes("youtube.com") || url.includes("youtu.be")){
+    alert("Link disalin. Tempelkan di yt1s");
     window.open("https://yt1s.com/", "_blank");
   }
   else{
